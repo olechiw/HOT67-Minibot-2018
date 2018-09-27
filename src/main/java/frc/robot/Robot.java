cc678169d435e27166e00c72342fd192822f51af
Olechiw <jakob.olechiw@gmail.com>
2018-09-27 18:22:11 -0400

    Added deadband/pigeon

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,26 +7,33 @@
 
 package frc.robot;
 
+import java.util.function.BiFunction;
+
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.sensors.PigeonIMU;
+
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
 public class Robot extends IterativeRobot {
 
-    public static final int P = 0;
-    public static final int I = 0;
-    public static final int D = 0;
+    public static final int TALON_LEFT = 1;
+    public static final int TALON_RIGHT = 4;
 
-    public static final int TALON_LEFT = 0;
-    public static final int TALON_RIGHT = 1;
+    public static final int PIGEON = 2;
 
     public static final int JOYSTICK_DRIVER = 0;
     public static final int JOYSTICK_LY = 0;
     public static final int JOYSTICK_RX = 4;
 
+    public static final double LEFT_DEADBAND = .2;
+    public static final double RIGHT_DEADBAND = .2;
+
     WPI_TalonSRX leftTalon = new WPI_TalonSRX(TALON_LEFT);
     WPI_TalonSRX rightTalon = new WPI_TalonSRX(TALON_RIGHT);
+
+    PigeonIMU pigeon = new PigeonIMU(2);
 
     Joystick driver = new Joystick(JOYSTICK_DRIVER);
 
@@ -36,6 +43,7 @@ public class Robot extends IterativeRobot {
     public void robotInit() {
         drive = new DifferentialDrive(leftTalon, rightTalon);
 
+        // Inverting left, may have to change
         leftTalon.setInverted(true);
         rightTalon.setInverted(false);
     }
@@ -43,8 +51,16 @@ public class Robot extends IterativeRobot {
 
     @Override
     public void robotPeriodic() {
-        double driveSpeed = driver.getRawAxis(0);
-        double driveTurn = driver.getRawAxis(4);
+        BiFunction<Double, Double, Double> getInputWithDeadband = (input, deadband) -> {
+            if ((-deadband < input) && (input < deadband))
+                return 0.0;
+            else
+                return (input / (1 - deadband)); // Scale to 0-1
+        };
+
+        double driveSpeed = getInputWithDeadband.apply(driver.getRawAxis(0), LEFT_DEADBAND);
+        double driveTurn = getInputWithDeadband.apply(driver.getRawAxis(4), RIGHT_DEADBAND);
+
         drive.arcadeDrive(driveSpeed, driveTurn);
     }
 
