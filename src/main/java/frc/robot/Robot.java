424d627d24b51c3fee658e0034037f8c3e090b65
Olechiw <jakob.olechiw@gmail.com>
2018-10-04 12:31:45 -0400

    Switch to CANTalon demo (arcadedrive was funky)

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,92 +1,65 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package frc.robot;
 
-import java.util.function.BiFunction;
-
+import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.motorcontrol.DemandType;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends IterativeRobot {
-
-    public static final int TALON_LEFT = 13;
-    public static final int TALON_RIGHT = 0;
-
-    public static final int JOYSTICK_DRIVER = 0;
-    public static final int JOYSTICK_LY = 1;
-    public static final int JOYSTICK_RX = 4;
-
-
-    WPI_TalonSRX leftTalon = new WPI_TalonSRX(TALON_LEFT);
-    WPI_TalonSRX rightTalon = new WPI_TalonSRX(TALON_RIGHT);
-
-    Joystick driver = new Joystick(JOYSTICK_DRIVER);
-
-    DifferentialDrive drive;
+    /** Hardware */
+    TalonSRX _leftMaster = new TalonSRX(13);
+    TalonSRX _rightMaster = new TalonSRX(0);
+    Joystick _gamepad = new Joystick(0);
 
     @Override
     public void robotInit() {
-        drive = new DifferentialDrive(leftTalon, rightTalon);
-
-        // Inverting may have been screwing things up
-        /*
-        leftTalon.setInverted(false);
-        rightTalon.setInverted(true);
-        */
-
-        leftTalon.selectProfileSlot(0, 0);
-        rightTalon.selectProfileSlot(0, 0);
-
-        leftTalon.set(ControlMode.PercentOutput, 0.0);
-        rightTalon.set(ControlMode.PercentOutput, 0.0);
+        /* Don't use this for now */
     }
-
-
+    
     @Override
-    public void robotPeriodic() {
-        BiFunction<Double, Double, Double> getInputWithDeadband = (input, deadband) -> {
-            if ((-deadband < input) && (input < deadband))
-                return 0.0;
-            else
-                return input; // Scale to 0-1
-        };
-
+    public void teleopInit(){
+        /* Disable motor controllers */
+        _rightMaster.set(ControlMode.PercentOutput, 0);
+        _leftMaster.set(ControlMode.PercentOutput, 0);
         
-        // May have to invert driveturn/drivespeed
-        double driveSpeed = getInputWithDeadband.apply(driver.getRawAxis(JOYSTICK_LY), .2);
-        double driveTurn = getInputWithDeadband.apply(driver.getRawAxis(JOYSTICK_RX), .2);
-
-        SmartDashboard.putNumber("driveSpeed", driveSpeed);
-        SmartDashboard.putNumber("driveTurn", driveTurn);
-
+        /* Set Neutral mode */
+        _leftMaster.setNeutralMode(NeutralMode.Brake);
+        _rightMaster.setNeutralMode(NeutralMode.Brake);
         
-        drive.arcadeDrive(driveSpeed, driveTurn);
+        /* Configure output direction */
+        _leftMaster.setInverted(false);
+        _rightMaster.setInverted(true);
+        
+        System.out.println("This is a basic arcade drive using Arbitrary Feed Forward.");
+    }
+    
+    @Override
+    public void teleopPeriodic() {        
+        /* Gamepad processing */
+        double forward = -1 * _gamepad.getY();
+        double turn = _gamepad.getRawAxis(4)/.5;        
+        forward = Deadband(forward);
+        turn = Deadband(turn);
+
+        /* Basic Arcade Drive using PercentOutput along with Arbitrary FeedForward supplied by turn */
+        _leftMaster.set(ControlMode.PercentOutput, forward, DemandType.ArbitraryFeedForward, +turn);
+        _rightMaster.set(ControlMode.PercentOutput, forward, DemandType.ArbitraryFeedForward, -turn);
     }
 
-    @Override
-    public void autonomousInit() {
-
-    }
-
-    @Override
-    public void autonomousPeriodic() {
-    }
-
-    @Override
-    public void teleopPeriodic() {
-    }
-
-    @Override
-    public void testPeriodic() {
+    /** Deadband 5 percent, used on the gamepad */
+    double Deadband(double value) {
+        /* Upper deadband */
+        if (value >= +0.05) 
+            return value;
+        
+        /* Lower deadband */
+        if (value <= -0.05)
+            return value;
+        
+        /* Outside deadband */
+        return 0;
     }
 }
